3fb6a848e5d5d175130f0a8f17b7f33304dfa26a
neverulhaha <f4ntazer911@gmail.com>
2025-05-15 21:33:16 +0300

    Карта 2

--- src/components/LunarMap.tsx
+++ src/components/LunarMap.tsx
@@ -1,28 +1,33 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+// src/components/LunarMap.tsx
+import { MapContainer, WMSTileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-const mapStyle: React.CSSProperties = {
-  height: '400px',
+const mapStyle = {
+  height: '350px',
   width: '100%',
   borderRadius: '1.5rem',
-  marginTop: '1rem'
+  marginBottom: '1.5rem',
+  boxShadow: '0 0 24px 0 rgba(0,0,0,0.1)'
 };
 
 export default function LunarMap() {
   return (
     <MapContainer
       center={[0, 0]}
-      zoom={3}
-      minZoom={1}
-      maxZoom={8}
+      zoom={2}
+      minZoom={0}
+      maxZoom={7}
       style={mapStyle}
       scrollWheelZoom={true}
+      crs={undefined}
     >
-      <TileLayer
-        attribution='NASA/USGS'
-        url="https://ode.rsl.wustl.edu/moon/content/tiles/basemap/{z}/{x}/{y}.png"
-        maxZoom={8}
-        minZoom={1}
+      <WMSTileLayer
+        url="http://luna.iaaras.ru/lunaserv/wms"
+        layers="Lunar_LRO_LOLA_DEM_118m"
+        format="image/png"
+        transparent={true}
+        version="1.1.1"
+        attribution="IAARAS LunaServ"
       />
     </MapContainer>
   );
