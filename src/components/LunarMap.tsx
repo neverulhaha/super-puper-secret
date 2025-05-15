6a234a46453d99d942fc37bdb14c7cc10c5c3b62
neverulhaha <f4ntazer911@gmail.com>
2025-05-15 22:08:10 +0300

    Карта 6

--- src/components/LunarMap.tsx
+++ src/components/LunarMap.tsx
@@ -18,14 +18,16 @@ export default function LunarMap() {
       maxZoom={7}
       style={mapStyle}
       scrollWheelZoom={true}
+      worldCopyJump={false}
+      maxBounds={[[90, -180], [-90, 180]]}
     >
       <TileLayer
-  attribution="NASA MoonTrek"
-  url="https://trek.nasa.gov/tiles/Moon/EQ/LRO_WAC_Mosaic_Global_303ppd_v02/1.0.0//default/default028mm/{z}/{x}/{y}.jpg"
-  maxZoom={7}
-  minZoom={0}
-/>
-
+        attribution="NASA MoonTrek"
+        url="https://trek.nasa.gov/tiles/Moon/EQ/LRO_WAC_Mosaic_Global_303ppd_v02/{z}/{x}/{y}.png"
+        maxZoom={7}
+        minZoom={0}
+        noWrap={true}
+      />
     </MapContainer>
   );
 }
