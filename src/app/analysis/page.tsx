252ebae5cb0cfc576bda736fb3dcc973f9e08e87
neverulhaha <f4ntazer911@gmail.com>
2025-05-16 17:43:29 +0300

    Анализ 4

--- src/app/analysis/page.tsx
+++ src/app/analysis/page.tsx
@@ -368,14 +368,7 @@ export default function AnalysisPage() {
                 style={{ display: "none" }}
                 onChange={handleImportFile}
               />
-              <label
-                htmlFor="import-file-single"
-                className="mt-3 w-full rounded-xl bg-gray-200 hover:bg-gray-300 transition text-gray-700 font-semibold py-2 shadow cursor-pointer flex items-center justify-center"
-                onClick={() => importFileSingleRef.current?.click()}
-              >
-                <ArrowUpTrayIcon className="w-5 h-5 mr-2 rotate-180" />
-                Импортировать анализ
-              </label>
+
             </div>
             <button
               className="mt-4 w-full rounded-xl bg-blue-600 hover:bg-blue-700 transition text-white font-semibold py-2 shadow"
