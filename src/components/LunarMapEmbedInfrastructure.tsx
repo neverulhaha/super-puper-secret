5c330f73bdc43f1e33aa099e2faf75bd588dcdcc
neverulhaha <f4ntazer911@gmail.com>
2025-05-21 20:08:16 +0300

    Пути

--- src/components/LunarMapEmbedInfrastructure.tsx
+++ src/components/LunarMapEmbedInfrastructure.tsx
@@ -380,7 +380,8 @@ export default function LunarMapEmbedInfrastructure({
   const end = points[points.length - 1]
   const prev = points[points.length - 2]
   const dir = new THREE.Vector3().subVectors(end, prev).normalize()
-  const arrowHelper = new THREE.ArrowHelper(dir, end, lineColor, 0.07, 0.05)
+  const arrowLength = 0.05
+  const arrowHelper = new THREE.ArrowHelper(dir, end, arrowLength, lineColor)
   group.add(arrowHelper)
 }
   if (typeof animationPoint === 'object' && animationPoint !== null) {
